feecbf92bcf2c035dffdb128b01bd4b1cff839cc
ynsgnc <115088362+ynsgnc@users.noreply.github.com>
2023-09-04 13:38:48 +0300

    navbar was added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./component/Navbar";
 import "./styles/tailwind.css";
+import MyNavbar from "./component/Navbar";
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar />
+    <ThemeProvider>
+      <MyNavbar />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
@@ -8,4 +13,4 @@ export default {
     extend: {},
   },
   plugins: [],
-};
+});
