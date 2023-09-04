d5518a5d269272e1e87ae5066a1c4f9b19a100b1
ynsgnc <115088362+ynsgnc@users.noreply.github.com>
2023-09-04 11:21:14 +0300

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <div className="container mx-auto">
+      <div class="flex">
+        <div class="flex-auto bg-red-500 w-32">01</div>
+        <div class="flex-auto bg-black text-white w-64">02</div>
+        <div class="flex-auto bg-slate-500   w-32">03</div>
+      </div>
+    </div>
+  );
+}
